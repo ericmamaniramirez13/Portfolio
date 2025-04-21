ab28ed087f99a1a12e3b00a7e7ec1003ff165fcd
ericmamaniramirez13 <ericmamaniramirez13@gmail.com>
2025-04-20 23:43:53 -0700

    Add NavBar to portfolio. Need to complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
-import ParticlesBackground from './components/BackgroundParticles'
+import React from 'react';
+import Navbar from './components/NavBar';
+import ParticlesBackground from './components/ParticlesBackground';
 
 function App() {
   return (
-    <div >
+    <div>
       <ParticlesBackground />
+      <Navbar />
       <div className="content">
-        <h1>Hi, I'm Eric 👨‍💻</h1>
-        <p>Welcome to my dynamic portfolio!</p>
+        <h1>Hi, I'm Eric a aspiring Software Engineer</h1>
+        <main className="pt-20 text-white">
+          <section id="home" className="h-screen bg-gray-900 flex justify-center items-center">
+            <h1 className="text-4xl">Welcome to Eric's Portfolio</h1>
+          </section>
+          {/* Add more sections here... */}
+        </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
