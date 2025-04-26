680efc816a6e53757fe4987f6e4e9d2d0a54a1c9
ericmamaniramirez13 <ericmamaniramirez13@gmail.com>
2025-04-26 02:04:20 -0700

    Add content for About, Experience, Resume, Contact. Upload logos svg files and resume .pdf holding my resume. Need to work on responsiveness, ex. mobile view. Also, It lacks content for Projects and Skills. Edit the navbar and add title on the upper left side.

---
+++ src/components/Experience.jsx
@@ -0,0 +1,59 @@
+import amazonLogo from '../assets/Amazon_logo.svg';
+import paypalLogo from '../assets/PayPal_logo.svg';
+import gmLogo from '../assets/GM_logo.svg';
+import '../Experience.css';
+const experiences = [
+    {
+      company: 'PayPal',
+      logo: paypalLogo,
+      role: 'Software Engineer Intern',
+      time: 'Fall 2025 (Offer Extended)',
+      description:
+        'Will contribute to backend infrastructure and help streamline payment pipelines across scalable distributed systems.',
+    },
+    {
+        company: 'Amazon',
+        logo: amazonLogo,
+        role: 'Software Engineer Intern',
+        time: 'Summer 2024 (Offer Extended)',
+        description:
+          "I'll work at Amazon in the store organizacion. Details are yet to be determined.",
+      },
+    {
+      company: 'General Motors',
+      logo: gmLogo,
+      role: 'Software Engineer Intern',
+      time: 'May 2024 – Oct 2024, Austin, TX',
+      description:
+        'Increased test coverage to 95% with Java + TestNG. Led full-stack development with React and Python. Agile team collaboration every sprint.',
+    },
+    {
+        company: 'Amazon',
+        logo: amazonLogo,
+        role: 'Software Engineer Intern',
+        time: 'May 2023 – July 2023, Tempe, AZ',
+        description:
+          'Developed a new UI using React, Angular, and AWS. Improved error feedback for deal validation, and implemented API integrations to automate backend checks.',
+      },
+  ];
+
+const Experience = () => {
+  return (
+    <section className="experience-container">
+      <h3>🚀 Work Experience</h3>
+      {experiences.map((exp, idx) => (
+        <div className="experience-card" key={idx}>
+          <div className="text-section">
+            <h3>{exp.role} @ {exp.company}</h3>
+            <p className="exp-time">{exp.time}</p>
+            {/*<p>{exp.description}</p>*/}
+          </div>
+          <div className="logo-section">
+            <img src={exp.logo} alt={`${exp.company} logo`} className="company-logo" />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
+export default Experience;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,15 @@
+import '../Resume.css';
+
+const Resume = () => {
+    return (
+      <div className="resume-container">
+        <object data="EricMamaniPortfolio.pdf" type="application/pdf" width="100%" height="980"></object>
+        <a href="EricMamaniPortfolio.pdf" target="_blank" rel="noopener noreferrer" download>
+          <button>Download Resume</button>
+        </a>
+        <p>Click the button below to download my resume.</p>
+      </div>
+    );
+  };
+  
+  export default Resume;
